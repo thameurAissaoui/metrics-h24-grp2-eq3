3f4ea91745a5f5bca8d3d3f938161a3dcd9994fd
louis-antoine-etsmtl <louis-antoine.caron.1@ens.etsmtl.ca>
2024-01-30 11:16:54 -0500

    ajout routeurs

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,17 @@
-const express = require('express');
-const metriquesKanbanRouter = require("./routers/metriquesKanban.mjs");
+import express from 'express';
+
+import metriquesKanbanRouter from './routers/metriquesKanban.mjs';
+import metriquesPullRequestsRouter from './routers/metriquesPullRequests.mjs';
+import metriquesVisualisationRouter from './routers/metriquesVisualisation.mjs';
 
 const PORT = process.env.NODE_PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.use(metriquesKanbanRouter);
+
+app.use("/metriquesKanban", metriquesKanbanRouter);
+app.use("/metriquesPullRequests", metriquesPullRequestsRouter);
+app.use("/snapshot", metriquesVisualisationRouter);
 
 app.listen(PORT, () => {
     console.log(`Serveur écoutant sur le port ${PORT}`)
